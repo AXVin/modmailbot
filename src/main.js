da348bfd531350fef92015eae982a7944936ad2b
AXVin <35896758+AXVin@users.noreply.github.com>
2018-12-29 19:19:57 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -27,17 +27,7 @@ const attachments = require("./data/attachments");
 const {ACCIDENTAL_THREAD_MESSAGES} = require('./data/constants');
 
 // The snippet to keep the application alive(pings every 5 minutes)
-const http = require('http');
-const express = require('express');
-const app = express();
-app.get("/", (request, response) => {
-console.log(Date.now() + " Ping Received");
-response.sendStatus(200);
-});
-app.listen(process.env.PORT);
-setInterval(() => {
-http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-}, 280000);
+
 
 
 
